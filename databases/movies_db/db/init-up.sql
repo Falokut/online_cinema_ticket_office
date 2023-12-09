f4ec7620dafbc3b4c918fab11470881643756e52
Falokut <71385842+Falokut@users.noreply.github.com>
2023-12-09 23:23:04 +0300

    Update movies_service

--- databases/movies_db/db/init-up.sql
+++ databases/movies_db/db/init-up.sql
@@ -7,13 +7,26 @@ CREATE TABLE movies (
     title_ru TEXT NOT NULL,
     title_en TEXT,
     budget TEXT,
-    plot TEXT NOT NULL,
+    description TEXT NOT NULL,
+    short_description TEXT NOT NULL,
     directors  INT ARRAY,
     genres  INT ARRAY,
     countries  INT ARRAY, 
     duration INT NOT NULL,
     poster_picture_id TEXT,
+    background_picture_id TEXT,
+    preview_poster_picture_id TEXT,
     cast_id INT NOT NULL,
+    age_rating TEXT NOT NULL,
     release_year SMALLINT NOT NULL
 );
+
+
+CREATE TABLE age_ratings (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+)
+
+CREATE INDEX cast_id_1702143699941_index ON "movies" USING btree ("cast_id");
 GRANT SELECT ON movies TO movies_service;
+GRANT SELECT ON age_ratings TO movies_service;
